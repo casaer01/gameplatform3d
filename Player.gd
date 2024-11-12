9343d34326e8b9d5c1928a08e198dcf97025a44b
Eros Casas <ecasas182015@gmail.com>
2024-11-11 19:58:29 -0600

    added jump function & game over

--- Player.gd
+++ Player.gd
@@ -9,12 +9,19 @@ var facing_angle: float
 @onready var model : MeshInstance3D = get_node("Model") 
 
 func _physics_process(delta: float) -> void:
+	
+	#pull player down when in air
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	#jump function only on floor
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+		velocity.y = jump_force
 	
 	var input = Input.get_vector("move_left","move_right","move_forward","move_backward")
 	var dir = Vector3(input.x,0, input.y)
 	
+	#direction to velocity
 	velocity.x = dir.x * move_speed
 	velocity.z = dir.z * move_speed
 	
@@ -25,3 +32,9 @@ func _physics_process(delta: float) -> void:
 		facing_angle = Vector2(input.y, input.x).angle()
 	
 	model.rotation.y = lerp_angle(model.rotation.y, facing_angle, 0.5)
+	
+	if global_position.y < -5:
+		game_over()
+
+func game_over():
+	get_tree().reload_current_scene()
