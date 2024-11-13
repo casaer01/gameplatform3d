40bb8525f85f9d49d75a6163d7133dd90fdb59f6
Eros Casas <ecasas182015@gmail.com>
2024-11-13 13:19:30 -0600

    added game over on enemy touched

--- enemy.gd
+++ enemy.gd
@@ -20,3 +20,8 @@ func _process(delta: float) -> void:
 			target_pos = start_pos + move_dir
 		else:
 			target_pos = start_pos
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		body.game_over()
